cd06f81c50a8aed08614f1780fc265dfc5ec0a81
Krvknv <cristina.krivokoneva@yandex.ru>
2022-12-11 22:35:35 +0300

    refactor: change divider component

--- src/components/divider/Divider.tsx
+++ src/components/divider/Divider.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Typography } from 'antd';
 
 import styles from './divider.module.css';
 
 const { Text, Title } = Typography;
 
-const Divider = () => {
+const Divider = (props: { children: ReactNode }) => {
+  const { children } = props;
   return (
     <div className={styles.divider}>
-      <Title style={{ color: '#fff' }}>BLOG</Title>
-      <Text className={styles.subtitle}>A place to share your knowledge</Text>
+      {/* <Title style={{ color: '#fff' }}>BLOG</Title>
+      <Text className={styles.subtitle}>A place to share your knowledge</Text> */}
+      {children}
     </div>
   );
 };
